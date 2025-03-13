f14f24aa33240d00515e1df3a052b170ed177ed1
BHznJNs <441768875@qq.com>
2025-03-13 19:57:24 +0800

    removed log

--- frontend/scripts/pathManager.js
+++ frontend/scripts/pathManager.js
@@ -56,7 +56,6 @@ export async function hashChangeEvent(e) {
     if (pathManager.isIn.directory()) {
         // open directory
         const index = await fetchJSON(getIndexPathFromHash())
-        console.log(index)
         if (!index) return
         newestHeader.classList.add("hidden")
         directoryHeader.classList.remove("hidden")
